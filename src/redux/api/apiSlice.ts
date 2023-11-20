ba55e1e2d2874ebe9e9a46a460942861ea117717
hosan30 <50867996+hosan30@users.noreply.github.com>
2023-11-20 18:59:41 +0600

    Backend Url Updated

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -3,7 +3,9 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://olloyophotos.onrender.com/api/v1',
+    // baseUrl: 'https://olloyophotos.onrender.com/api/v1',
+    baseUrl: 'https://olloyophotos-backend.vercel.app/api/v1',
+    // baseUrl: 'http://localhost:5000/api/v1',
     // prepareHeaders: (headers, {getState}) => {
     //   const {auth :{
     //     accessToken
